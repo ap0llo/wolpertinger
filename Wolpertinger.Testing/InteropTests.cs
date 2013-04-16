4364e16aeb938dbc188b43ce4f8e8ee3b62c07fd
Andreas Grünwald <andi@nerdcave.eu>
2013-04-16 21:20:49 +0200

    Added InteropTest to test (not extensivelsy at all) deserialization of xml generated by a other program implementing parts of wolpertinger

---
+++ Wolpertinger.Testing/InteropTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nerdcave.Common.Xml;
+using Wolpertinger.FileShareCommon;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Wolpertinger.Testing
+{
+    /// <summary>
+    /// Tests for testing interop with alternate wolpertinger implemtation
+    /// </summary>
+    [TestClass]    
+    public class InteropTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            XmlSerializer.RegisterType(typeof(DirectoryObject), "directoryObject");
+        }
+
+        [TestMethod]
+        public void DeserializationTest1()
+        {
+            string input = File.ReadAllText("../f.xml");
+            var xml = XElement.Parse(input);
+
+            var dir = XmlSerializer.Deserialize(xml) as DirectoryObject;
+        }
+    }
+}
